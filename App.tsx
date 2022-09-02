ec1bb1791c036fe1816279fbfe54cfea7727a7dd
TaniaCruz09 <cruztania0909@gmail.com>
2022-09-02 03:53:23 -0600

    proyect

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Conversor from "./src/screen/Convertor";
+
+export default function App() {
+  return <Conversor />;
+}
